0d585079e5790c690cad088629dd1cff7a85fb9c
minelou <emamine90@gmail.com>
2023-04-03 19:41:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\z", sizeof(f));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
